6dfa30bd8aeb945a272f2f9c435fd79aea5859a9
Tong Napol <napol@unixdev.co.th>
2021-06-27 14:41:28 +0700

    Add comma and revise spacing/tab

--- index.js
+++ index.js
@@ -1,101 +1,100 @@
-/** Zeros Origin index */
+/** Zeros Origin Index. */
 
-const WebSocket = require('ws')
-const http = require('http')
-const path = require('path')
-const express = require('express')
-const nobots = require('express-nobots')
-const favicon = require('serve-favicon')
-const app = express()
-const ngrok = require('ngrok')
-const main = require('./modules/origin')
-const server = http.createServer(app)
-const wss = new WebSocket.Server({ server: server })
+const WebSocket = require( 'ws' );
+const http = require( 'http' );
+const path = require( 'path' );
+const express = require( 'express' );
+const nobots = require( 'express-nobots' );
+const favicon = require( 'serve-favicon' );
+const app = express();
+const ngrok = require( 'ngrok' );
+const main = require( './modules/origin' );
+const server = http.createServer( app );
+const wss = new WebSocket.Server( { server: server } );
 
-var port = process.env.PORT || 0
+var port = process.env.PORT || 0;
 
-require('./routes/main')(path, app, main)
+require( './routes/main' )( path, app, main );
 
-app.get('*', function(req, res, next){
-  if (!req.headers.host.includes("localhost")) {
-    if (req.get('x-forwarded-proto') != "https") {
-        res.set('x-forwarded-proto', 'https')
-        res.redirect('https://' + req.get('host') + req.url)
-    } else {
-        next()   
-    }
-  } else {
-    next()
-  }
-})
+app.get( '*', function( req, res, next ) {
+	if (!req.headers.host.includes("localhost")) {
+		if (req.get('x-forwarded-proto') != "https") {
+			res.set('x-forwarded-proto', 'https');
+			res.redirect('https://' + req.get('host') + req.url);
+		} else {
+			next();
+		}
+  	} else {
+		next();
+  	}
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/html/index.html'))
-    main.createNode()
-    if (!req.headers.host.includes("localhost")) {
-      let wsp = new WebSocket('ws://origin.zeros.run')
-      wsp.on('open', function open() {
-          console.log('Peer sent: ' + req.headers.host)
-          wsp.send(req.headers.host)
-      })
-      wsp.on('error', (error) => {
-          console.log('client error', error)
-      })
+app.get( '/', ( req, res ) => {
+    res.sendFile (path.join( __dirname + '/html/index.html') );
+    main.createNode();
+    if ( !req.headers.host.includes( "localhost" ) ) {
+    	let wsp = new WebSocket( 'ws://origin.zeros.run' );
+      	wsp.on('open', function open() {
+        	console.log( 'Peer sent: ' + req.headers.host );
+        	wsp.send( req.headers.host );
+      	})
+      	wsp.on( 'error', ( error ) => {
+    		console.log('client error', error);
+      	})
     }
 })
 
 /** Start ngrok for access to local. */
-app.get('/start-ngrok', (req, res) => {
-  let url = '';
-  if (req.headers.host.includes("localhost")) {
-    (async function() {
-      url = await ngrok.connect(req.socket.localPort)
-      res.sendFile(path.join(__dirname + '/html/ngrok.html'))
-      let wsp = new WebSocket('ws://origin.zeros.run')
-      wsp.on('open', function open() {
-          console.log('Peer sent: ' + url)
-          wsp.send(url)
-      })
-      wsp.on('error', (error) => {
-          console.log('client error', error)
-      })
-    })()
-  }
-  else {
-    res.sendFile(path.join(__dirname + '/html/no-ngrok.html'))
-  }
-})
+app.get( '/start-ngrok', ( req, res ) => {
+  	let url = '';
+	if ( req.headers.host.includes("localhost") ) {
+		( async function() {
+			url = await ngrok.connect(req.socket.localPort);
+			res.sendFile(path.join(__dirname + '/html/ngrok.html'));
+			let wsp = new WebSocket('ws://origin.zeros.run');
+			wsp.on('open', function open() {
+				console.log('Peer sent: ' + url);
+				wsp.send(url);
+			})
+			wsp.on('error', (error) => {
+				console.log('client error', error);
+			})
+   		} )()
+  	} else {
+    	res.sendFile(path.join(__dirname + '/html/no-ngrok.html'));
+  	}
+});
 
-// Disconnect from ngrok server.
-app.get('/disconnect', (req, res) => {
-  if (req.headers.host.includes("localhost")) {
-    (async function() {
-      await ngrok.disconnect()
-    })()
-    res.sendFile(path.join(__dirname + '/html/disconnect.html'))
-  }
-})
+/** Disconnect from ngrok server. */
+app.get( '/disconnect', ( req, res ) => {
+	if (req.headers.host.includes( "localhost" )) {
+    	(async function() {
+      		await ngrok.disconnect();
+    	})()
+    	res.sendFile( path.join(__dirname + '/html/disconnect.html' ));
+  	}
+});
 
 /** Add favicon and block robots. */
-app.use(nobots({block:true}))
-app.use(favicon(__dirname + '/favicon.ico'))
-app.use(express.static('html'));
+app.use( nobots({block:true}) );
+app.use( favicon(__dirname + '/favicon.ico') );
+app.use( express.static('html') );
 
 /** Show console log when get a message. */
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('Server received: %s', message + '\n')
-  })
-})
+wss.on( 'connection', function connection( ws ) {
+	ws.on( 'message', function incoming( message ) {
+    	console.log( 'Server received: %s', message + '\n' );
+  	});
+});
 
 /** Echo error. */
 wss.on('error', (error) => {
-  console.log('client error', error)
+	console.log('client error', error);
 })
 
 /** Listen server. */
 server.listen(port, () => {
-  console.log('\nZeros Origin Network is running... (Ctrl + c to exit)\n')
-  console.log('Socket port: '+ server.address().port +'\n')
-  port = server.address().port
-})
+	console.log( '\nZeros Origin Network is running... (Ctrl + c to exit)\n' );
+	console.log( 'Socket port: '+ server.address().port +'\n' );
+  	port = server.address().port;
+});
--- modules/origin.js
+++ modules/origin.js
@@ -1,43 +1,43 @@
-/** Zeros Origin main */
+/** Zeros Origin Main. */
 
-const WebSocket = require('ws')
-const SHA256 = require('crypto-js/sha256')
-const fs = require('fs')
+const WebSocket = require( 'ws' );
+const SHA256 = require( 'crypto-js/sha256' );
+const fs = require( 'fs' );
 
-var nodeID = 'NULL'
-var randomString = randomString()
-var dir = __dirname + '/../model/'
+var nodeID = 'NULL';
+var randomString = randomString();
+var dir = __dirname + '/../model/';
 
 /** Random string for id creation. */
 function randomString() {
-    let length = 80
-    let result = ''
-    let all = 'zerosZEROS01'
-    var allLength = all.length
+    let length = 80;
+    let result = '';
+    let all = 'zerosZEROS01';
+    var allLength = all.length;
     for ( var i = 0; i < length; i++ ) {
-       result += all.charAt(Math.floor(Math.random() * allLength))
+       result += all.charAt( Math.floor( Math.random() * allLength) );
     }
-    return result
+    return result;
  }
 
 /** Create node and save id in model folder. */
 function createNode () {
-    if (!fs.existsSync(dir)){
-        fs.mkdirSync(dir)
-        fs.open(dir + 'identity','r',function(err, fd){
-            if (err) {
-                obj = JSON.parse('{}')
-                if (Object.keys(obj).length === 0) {
-                    nodeID = SHA256(randomString).toString()
-                    console.log('Node ID: ' + nodeID + '\n')
-                    let input = JSON.parse('{"node_id":"'+ nodeID +'"}')
-                    json = JSON.stringify(input)
-                    fs.writeFile(dir + 'identity', json, (err) => {
-                        if (err) throw err
-                        console.log('Saved node id\n')
+    if ( !fs.existsSync(dir) ){
+        fs.mkdirSync( dir );
+        fs.open( dir + 'identity','r',function( err, fd ){
+            if ( err ) {
+                obj = JSON.parse( '{}' );
+                if ( Object.keys(obj).length === 0 ) {
+                    nodeID = SHA256( randomString).toString();
+                    console.log( 'Node ID: ' + nodeID + '\n' );
+                    let input = JSON.parse( '{"node_id":"'+ nodeID +'"}' );
+                    json = JSON.stringify( input );
+                    fs.writeFile( dir + 'identity', json, ( err ) => {
+                        if ( err ) throw err;
+                        console.log( 'Saved node id\n' );
                     })
                 }
-                registerPeer(nodeID)
+                registerPeer( nodeID );
             }
         })
     }
@@ -45,48 +45,48 @@ function createNode () {
 
 /** Register peer to Zeros Origin. */
 function registerPeer(id) {
-    let wsp = new WebSocket('ws://origin.zeros.run')
+    let wsp = new WebSocket( 'ws://origin.zeros.run' );
     wsp.on('open', function open() {
-        wsp.send(id)
-        console.log('Peer sent: ' + id)
+        wsp.send( id );
+        console.log( 'Peer sent: ' + id );
     })
 
-    wsp.on('error', (error) => {
-        console.log('client error', error)
+    wsp.on( 'error', ( error ) => {
+        console.log( 'client error', error );
     })
 }
 
 /** Connect peer to Zeros Origin. */
-function connectPeer(address, id, req, res) {
-    let ws_scheme = ''
-    if (req.protocol == "https:") {
-        ws_scheme = "wss://"
+function connectPeer( address, id, req, res ) {
+    let ws_scheme = '';
+    if ( req.protocol == "https:" ) {
+        ws_scheme = "wss://";
     } else {
-        ws_scheme = "ws://"
+        ws_scheme = "ws://";
     }
-    let wsp = new WebSocket(ws_scheme + address)
-    wsp.on('open', function open() {
-        fs.readFile(dir + 'identity', 'utf8', function readFileCallback(err, data){
-            if (err){
-                console.log(err)
+    let wsp = new WebSocket( ws_scheme + address );
+    wsp.on( 'open', function open() {
+        fs.readFile( dir + 'identity', 'utf8', function readFileCallback( err, data ){
+            if ( err ) {
+                console.log( err );
             } else {
-                obj = JSON.parse(data)
-                json = JSON.stringify(obj)
-                if (json.includes(id)) {
-                    wsp.send(json)
-                    console.log('Peer sent: ' + json)
-                    res.send('Peer sent')
+                obj = JSON.parse( data );
+                json = JSON.stringify( obj );
+                if ( json.includes( id ) ) {
+                    wsp.send( json );
+                    console.log( 'Peer sent: ' + json );
+                    res.send( 'Peer sent' );
                     
                 } else {
-                    console.log('denied')
-                    res.send('denied')
+                    console.log( 'denied' );
+                    res.send( 'denied' );
                 }
             }
         })
     })
 
-    wsp.on('error', (error) => {
-        console.log('client error', error)
+    wsp.on( 'error', ( error ) => {
+        console.log( 'client error', error );
     })
 }
 
