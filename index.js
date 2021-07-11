89b9220ae4b06bfae02df69620b83b9929c2f9a3
zeros-origin <auctok@gmail.com>
2021-07-11 13:03:44 +0700

    Remove id from peers list if disconnect

--- index.js
+++ index.js
@@ -49,56 +49,79 @@ app.get( '/', ( req, res ) => {
 
 /** Start ngrok for access to local. */
 app.get( '/start-ngrok', ( req, res ) => {
-    let url = '';
-    if ( req.headers.host.includes("localhost") ) {
-        ( async function() {
-            url = await ngrok.connect(req.socket.localPort);
-            res.sendFile(path.join(__dirname + '/html/ngrok.html'));
-            let wsp = new webSocket('ws://origin.zeros.run');
-            wsp.on('open', function open() {
-                console.log('Peer sent: ' + url);
-                wsp.send(url);
-                /** Add peer to peers list. */
-                fs.readFile( dir + 'peers.json', 'utf8', (err, data) => {
-                    if ( err ) {
-                        let obj = JSON.parse( '{}' );
-                        if ( Object.keys(obj).length === 0 ) {
-                            let identityModel = fs.readFileSync( dir + 'identity.json' );
-                            let peerJson = JSON.parse(identityModel);
-                            let peerID = peerJson.peerID;
-                            console.log( 'Add peer ID: ' + peerID + '\n' );
-                            let origin = 'origin.zeros.run'
-                            let input = JSON.parse( '{"peerID":"'+ peerID +'", "url":"'+ url +'", "origin":"'+ origin +'"}' );
-                            json = JSON.stringify( input, null, 4 );
-                            fs.writeFile( dir + 'peers.json', json, ( err ) => {
-                                if ( err ) throw err;
-                                console.log( 'Saved peer to peers list.\n' );
-                            })
-                        }
-                    } else {
-                        let identityModel = fs.readFileSync( dir + 'identity.json' );
-                        let peerJson = JSON.parse(identityModel);
-                        let peerID = peerJson.peerID;
-                        console.log( 'Add peer ID: ' + peerID + '\n' );
-                        let origin = 'origin.zeros.run'
-                        let peersList = [];
-                        peersList.push( JSON.parse( data ) );
-                        peersList.push( { "peerID": peerID, "url": url, "origin": origin } );
-                        let json = JSON.stringify( peersList, null, 4 );
-                        fs.writeFile( dir + 'peers.json', json, ( err ) => {
-                            if ( err ) throw err;
-                            console.log( 'Saved peer to peers list.\n' );
+
+    fs.readFile( dir + 'peers.json', 'utf8', (err, data) => {
+        if ( err ) {
+            console.log( 'No peers list.\n' );
+        } else {
+            let identityModel = fs.readFileSync( dir + 'identity.json' );
+            let peerJson = JSON.parse(identityModel);
+            let peerID = peerJson.peerID;
+            let peersList = JSON.parse( data );
+            peersList = peersList.filter( function( peersList ) {
+                return peersList.peerID == peerID;
+            });
+            if ( Object.keys(peersList).length == 0 ) {
+                let url = '';
+                if ( req.headers.host.includes("localhost") ) {
+                    ( async function() {
+                        url = await ngrok.connect(req.socket.localPort);
+                        res.sendFile(path.join(__dirname + '/html/ngrok.html'));
+                        let wsp = new webSocket('ws://origin.zeros.run');
+                        wsp.on('open', function open() {
+                            console.log('Peer sent: ' + url);
+                            wsp.send(url);
+                            /** Add peer to peers list. */
+                            fs.readFile( dir + 'peers.json', 'utf8', (err, data) => {
+                                if ( err ) {
+                                    let obj = JSON.parse( '{}' );
+                                    if ( Object.keys(obj).length === 0 ) {
+                                        let identityModel = fs.readFileSync( dir + 'identity.json' );
+                                        let peerJson = JSON.parse(identityModel);
+                                        let peerID = peerJson.peerID;
+                                        console.log( 'Add peer ID: ' + peerID + '\n' );
+                                        let origin = 'origin.zeros.run'
+                                        let peersList = [];
+                                        let input = JSON.parse( '{"peerID":"'+ peerID +'", "url":"'+ url +'", "origin":"'+ origin +'"}' );
+                                        peersList.push( input );
+                                        let json = JSON.stringify( peersList, null, 4 );
+                                        fs.writeFile( dir + 'peers.json', json, ( err ) => {
+                                            if ( err ) throw err;
+                                            console.log( 'Saved peer to peers list.\n' );
+                                        })
+                                    }
+                                } else {
+                                    let identityModel = fs.readFileSync( dir + 'identity.json' );
+                                    let peerJson = JSON.parse(identityModel);
+                                    let peerID = peerJson.peerID;
+                                    console.log( 'Add peer ID: ' + peerID + '\n' );
+                                    let origin = 'origin.zeros.run'
+                                    let peersList = JSON.parse( data );
+                                    peersList = peersList.filter( function( peersList ) {
+                                        return peersList.peerID != peerID;
+                                    });
+                                    peersList.push( { "peerID": peerID, "url": url, "origin": origin } );
+                                    let json = JSON.stringify( peersList, null, 4 );
+                                    fs.writeFile( dir + 'peers.json', json, ( err ) => {
+                                        if ( err ) throw err;
+                                        console.log( 'Saved peer to peers list.\n' );
+                                    })
+                                }
+                            });
                         })
-                    }
-                });
-            })
-            wsp.on('error', (error) => {
-                console.log('client error', error);
-            })
-           } )();
-      } else {
-        res.sendFile(path.join(__dirname + '/html/no-ngrok.html'));
-      }
+                        wsp.on('error', (error) => {
+                            console.log( 'client error', error );
+                        })
+                    } )();
+                } else {
+                    res.sendFile(path.join(__dirname + '/html/no-ngrok.html'));
+                }
+            } else {
+                console.log( 'Already in peers list.' );
+                res.sendFile(path.join(__dirname + '/html/ngrok.html'));
+            }
+        }
+    });
 });
 
 /** Disconnect from ngrok server. */
@@ -106,6 +129,25 @@ app.get( '/close-ngrok', ( req, res ) => {
     if (req.headers.host.includes( "localhost" )) {
         (async function() {
             await ngrok.disconnect();
+            fs.readFile( dir + 'peers.json', 'utf8', (err, data) => {
+                if ( err ) {
+                    console.log( 'No peers list.\n' );
+                } else {
+                    let identityModel = fs.readFileSync( dir + 'identity.json' );
+                    let peerJson = JSON.parse(identityModel);
+                    let peerID = peerJson.peerID;
+                    console.log( 'Remove peer ID: ' + peerID + '\n' );
+                    let peersList = JSON.parse( data );
+                    peersList = peersList.filter( function( peersList ) {
+                        return peersList.peerID != peerID;
+                    });
+                    let json = JSON.stringify( peersList, null, 4 );
+                    fs.writeFile( dir + 'peers.json', json, ( err ) => {
+                        if ( err ) throw err;
+                        console.log( 'Removed peer to peers list.\n' );
+                    })
+                }
+            });
         })();
         res.sendFile( path.join(__dirname + '/html/disconnect.html' ));
     }
